ce67753144943f9c802afef23862835ea860234d
chinawin <chinawin47@gmail.com>
2026-03-15 22:27:08 +0700

    resubmit assignment

--- ex-4.js
+++ ex-4.js
@@ -1,15 +1,9 @@
 // Start coding here
-
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
-
-let calculator = {
-  add: add,
-  subtract: subtract,
-  multiply: multiply,
-  divide: divide,
+const calculator = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => a / b,
 };
 
 let addResult = calculator.add(10, 20);
